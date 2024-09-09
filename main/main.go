723e3299f591a85938a0f92c7133c639021a3db4
ulfaric <ryf0510@live.com>
2024-09-09 20:25:21 +0800

    updated

--- main/main.go
+++ main/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"log"
-	"github.com/ulfaric/gomodbus/wrapper"
+	"github.com/ulfaric/gomodbus/socket"
 )
 
 func main() {
-	w := wrapper.NewWrapper(1993)
+	w := socket.NewSocket(1993)
 	err := w.Start()
 	if err != nil {
 		log.Fatalf("Failed to start wrapper: %v", err)
